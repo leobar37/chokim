507766545bf5e87cf793cf8bc6f358a72fe51bf5
leobar37 <leonardito3458@gmail.com>
2021-08-31 22:24:30 -0500

    add slider library

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-
-import "./asssets/scss/main.scss"
+import "./asssets/scss/main.scss";
 // import "bulma"
 
+import Glide from "@glidejs/glide";
+
+new Glide(".glide", {
+  perView: 3,
+  type: "slider",
+  focusAt: 1,
+  gap: 10,
+  bound: true,
+}).mount();
